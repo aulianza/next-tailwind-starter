4af07a5c978889c31ba12dde727f8bf865427a97
Ryan Aulia <hello@aulianza.id>
2023-06-24 15:53:01 +0700

    delete: unnecessary folder

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"_app.tsx -> src/common/components/ProgressBar\":{\"id\":\"_app.tsx -> src/common/components/ProgressBar\",\"files\":[\"static/chunks/src_common_components_ProgressBar_index_ts.js\"]}}"
